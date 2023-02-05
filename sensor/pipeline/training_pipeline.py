19d5a85e5ec27d77e6016c87dc75d2f52533f010
Hitesh Nimbalkar <nimbalkarhitesh@gmail.com>
2023-02-06 01:53:44 +0530

    Model trainer 1.1

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -34,9 +34,16 @@ def start_training_pipeline():
         data_ingestion_artifact=data_ingestion_artifact)
         data_transformation_artifact = data_transformation.initiate_data_transformation()
 
-        ##model trainer
+        #model trainer
         model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
         model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
         model_trainer_artifact = model_trainer.initiate_model_trainer()
 
-        print("###################################")
+
+        #model evaluation
+        model_eval_config = config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
+        model_eval  = ModelEvaluation(model_eval_config=model_eval_config,
+        data_ingestion_artifact=data_ingestion_artifact,
+        data_transformation_artifact=data_transformation_artifact,
+        model_trainer_artifact=model_trainer_artifact)
+        model_eval_artifact = model_eval.initiate_model_evaluation()
